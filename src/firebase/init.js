d0bf2c2e191fd12fa2b0f7032ed696ed84e28688
kiprono <42340517+MeKCK@users.noreply.github.com>
2020-03-12 22:18:00 +0300

    :wrench: Add firebase config

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,14 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
-  authDomain: 'bento-starter.firebaseapp.com',
-  databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
-  storageBucket: 'bento-starter.appspot.com',
-  messagingSenderId: '458464977217'
+  apiKey: 'AIzaSyBZayTfWkaB3q32wn15YEYvL1hsideBX64',
+  authDomain: 'zanah-news-updates.firebaseapp.com',
+  databaseURL: 'https://zanah-news-updates.firebaseio.com',
+  projectId: 'zanah-news-updates',
+  storageBucket: 'zanah-news-updates.appspot.com',
+  messagingSenderId: '683935734855',
+  appId: '1:683935734855:web:9a3c9be7c5ed52a641a562',
+  measurementId: 'G-3YWVRFGR8C'
 }
 
 firebase.initializeApp(config)
